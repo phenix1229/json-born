4f4c5c8915511c3c964e3f609fde24cb59a7eedf
Colin Jaffe <balloonasaurus@gmail.com>
2019-12-16 12:49:21 -0500

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,23 @@
+# JSON Born
+
+Exploring accessing local JSON files.
+
+
+### Introduction
+
+We'll be exploring how to read and write to the local file system.
+
+
+### Step 1
+
+Make a file called `users.json` that contains the `users` array, but as JSON.
+
+
+### Step 2
+
+???
+
+
+### Step 3
+
+PROFIT
---
+++ main.js
@@ -0,0 +1,22 @@
+const users = [
+  {
+    index: 0,
+    age: 34,
+    eyeColor: 'green',
+    name: 'Prince Gould',
+    friends: [
+      {
+        id: 0,
+        name: 'Earlene Pollard'
+      },
+      {
+        id: 1,
+        name: 'Pittman Marquez'
+      },
+      {
+        id: 2,
+        name: 'Ruby Rogers'
+      }
+    ]
+  },
+]
